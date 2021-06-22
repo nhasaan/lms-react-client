a216289faaef05f2a285e03c842c9e137bacb12a
Nazmul Hassan <nazmul.phpdroidx@gmail.com>
2021-06-23 00:37:19 +0600

    signup for admin

---
+++ src/features/users/RegisterAdminForm.tsx
@@ -0,0 +1,39 @@
+import { ErrorMessage, Form, Formik } from 'formik';
+import { observer } from 'mobx-react-lite';
+import React from 'react';
+import { Button, Header } from 'semantic-ui-react';
+import MyTextInput from '../../app/common/form/MyTextInput';
+import { useStore } from '../../app/stores/store';
+import * as Yup from 'yup';
+import ValidationErrors from '../errors/ValidationErrors';
+
+export default observer(function RegisterAdminForm() {
+    const { userStore } = useStore();
+    return (
+        <Formik
+            initialValues={{ name: '', email: '', role: 'admin', password: '', error: null }}
+            onSubmit={(values, { setErrors }) => userStore.register(values).catch(error =>
+                setErrors({ error }))}
+            validationSchema={Yup.object({
+                name: Yup.string().required(),
+                email: Yup.string().required().email(),
+                password: Yup.string().required(),
+            })}
+        >
+            {({ handleSubmit, isSubmitting, errors, isValid, dirty }) => (
+                <Form className='ui form error' onSubmit={handleSubmit} autoComplete='off'>
+                    <Header as='h2' content='Sign up to LMS' color='teal' textAlign='center' />
+                    <MyTextInput name='name' placeholder='Name' />
+                    <MyTextInput name='email' placeholder='Email' />
+                    <MyTextInput name='password' placeholder='Password' type='password' />
+                    <ErrorMessage
+                        name='error' render={() =>
+                            <ValidationErrors errors={errors.error} />}
+                    />
+                    <Button disabled={!isValid || !dirty || isSubmitting}
+                        loading={isSubmitting} positive content='Register' type='submit' fluid />
+                </Form>
+            )}
+        </Formik>
+    )
+})
